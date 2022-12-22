9a4f02fc5511c8aedaafbd520dccda29a9a26e79
vpnchengo <charo.alvin@gmail.com>
2022-12-22 10:44:53 -0800

    corection

--- print_env.c
+++ print_env.c
@@ -1,12 +1,13 @@
 #include "shell.h"
 /**
  * print_env - prints the environment string to stdout
+ * @env: environmemnt
  * Return: Always 0
  */
 void print_env(char **env)
 {
 	int x = 0;
-	
+
 	while (env[x])
 	{
 		write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
